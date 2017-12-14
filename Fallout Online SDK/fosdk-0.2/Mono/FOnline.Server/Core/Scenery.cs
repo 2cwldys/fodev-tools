d55e81c912bf712732583e27558171a3310eff75
Stephen Payne <sdpayne113@gmail.com>
2017-12-14 17:22:26 -0500

    added rotators fosdk, 2238 content

---
+++ Fallout Online SDK/fosdk-0.2/Mono/FOnline.Server/Core/Scenery.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FOnline
+{
+    public partial class Scenery : IManagedWrapper
+    {
+        readonly IntPtr thisptr;
+        internal Scenery(IntPtr ptr)
+        {
+            thisptr = ptr;
+            AddRef();
+        }
+        ~Scenery()
+        {
+            Release();
+        }
+        public static explicit operator IntPtr(Scenery self)
+        {
+            return self != null ? self.ThisPtr : IntPtr.Zero;
+        }
+        public static explicit operator Scenery(IntPtr ptr)
+        {
+            return ptr != IntPtr.Zero ? new Scenery (ptr) : null;
+        }
+
+        public IntPtr ThisPtr { get { return thisptr; } }
+		public virtual void AddRef ()
+		{
+			AddRef (thisptr);
+		}
+		public virtual void Release()
+		{
+			Release (thisptr);
+		}
+    }
+    public sealed class SceneryArray : HandleArray<Scenery>
+    {
+        static readonly IntPtr type;
+        public SceneryArray()
+            : base(type)
+        {
+            Global.Log("Creating SceneryArray({0})", type);
+        }
+        internal SceneryArray (IntPtr ptr)
+            : base(ptr, true)
+	    {
+
+	    }
+        static SceneryArray()
+        {
+            type = ScriptArray.GetType("Scenery@[]");
+        }
+        public override Scenery FromNative(IntPtr ptr)
+        {
+            return (Scenery)GetObjectAddress(ptr);
+        }
+    }
+}
---
+++ Fallout Online SDK/rotator-tools/MacGyver/README.md
@@ -0,0 +1,4 @@
+MacGyver
+========
+
+This is an improved clone of SDK's MrFixit, a tool for working with crafting recipes.
